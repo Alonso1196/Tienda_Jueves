1aabbcf07cbf1e7a4a6686727bd45944fce53396
Alonso1196 <113742445+Alonso1196@users.noreply.github.com>
2025-03-19 00:18:40 -0600

    Categorias
    Ya me sirve las Categorias

---
+++ src/main/java/com/Tienda/controller/CategoriaController.java
@@ -0,0 +1,30 @@
+package com.Tienda.controller;
+
+
+import com.Tienda.domain.Categoria;
+import com.Tienda.service.CategoriaService;
+//import com.Tienda.service.impl.FirebaseStorageServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+@RequestMapping("/categoria")
+public class CategoriaController {
+  
+    @Autowired
+    private CategoriaService categoriaService;
+    
+    @GetMapping("/listado")
+    private String listado(Model model) {
+        var categorias = categoriaService.getCategorias(false);
+        model.addAttribute("categorias", categorias);
+        model.addAttribute("totalCategorias",categorias.size());
+        return "/categoria/listado";
+    }
+}
--- src/main/java/com/Tienda/service/CategoriaService.java
+++ src/main/java/com/Tienda/service/CategoriaService.java
@@ -1,4 +1,3 @@
-
 package com.Tienda.service;
 import com.Tienda.domain.Categoria;
 import java.util.List;
