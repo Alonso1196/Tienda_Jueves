c3f79cb431abf4a1852a1f9ae6ca8c6ac7df4e70
Alonso1196 <113742445+Alonso1196@users.noreply.github.com>
2025-04-16 21:57:32 -0600

    Semana 13
    Creación de los Roles
    Creación de Registro

--- src/main/java/com/Tienda/dao/ProductoDao.java
+++ src/main/java/com/Tienda/dao/ProductoDao.java
@@ -4,7 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
+
+
 public interface ProductoDao extends JpaRepository <Producto, Long>{
- public List<Producto> findByPrecioBetweenOrderByDescripcion(double
-precioInf, double precioSup);
+    
+    
+ public List<Producto> findByPrecioBetweenOrderByDescripcion(double precioInf, double precioSup);
+ 
+ 
 } 
---
+++ src/main/java/com/Tienda/domain/Rol.java
@@ -0,0 +1,29 @@
+package com.Tienda.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import java.io.Serializable;
+
+@Data
+@Entity
+@Table(name="rol")
+
+public class Rol implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_rol")
+    private Long idRol;
+    private String nombre;
+    @Column(name = "id_usuario")
+    private Long idUsuario;
+
+}
+
